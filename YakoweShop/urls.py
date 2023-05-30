e5c7cb53333037051c97e496fdba5a9418717c9c
Sta Rog <satach@o2.pl>
2023-05-30 14:17:26 +0200

    1

--- YakoweShop/urls.py
+++ YakoweShop/urls.py
@@ -25,3 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(apps.get_app_config('oscar').urls[0])),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
